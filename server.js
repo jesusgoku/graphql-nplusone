dbe95f784c06010f33c9d6a708c07f4e8e35aa9c
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-12-23 17:22:09 -0300

    split books and authors

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 const books = [
-  { title: 'Cinco Pepitas de Naranja', author: 'Sir Arthur Conan Doyle' },
-  { title: 'El Sabueso de los Baskerville', author: 'Sir Arthur Conan Doyle' },
-  { title: 'El Signo de los Cuatro', author: 'Sir Arthur Conan Doyle' },
-  { title: 'Escándalo en el Bohemia', author: 'Sir Arthur Conan Doyle' },
-  { title: 'Estudio en Escarlata', author: 'Sir Arthur Conan Doyle' },
+  { id: 1, title: 'Cinco Pepitas de Naranja', authorId: 1 },
+  { id: 2, title: 'El Sabueso de los Baskerville', authorId: 1 },
+  { id: 3, title: 'El Signo de los Cuatro', authorId: 1 },
+  { id: 4, title: 'Escándalo en el Bohemia', authorId: 1 },
+  { id: 5, title: 'Estudio en Escarlata', authorId: 1 },
 ];
+
+const authors = [{ id: 1, name: 'Sir Arthur Conan Doyle' }];
 
 const typeDefs = gql`
   type Book {
+    id: Int
     title: String
-    author: String
+    author: Author
+  }
+
+  type Author {
+    id: Int
+    name: String
+    books: [Book]
   }
 
   type Query {
     books: [Book]
+    authors: [Author]
   }
 `;
 
 const resolvers = {
   Query: {
     books: () => books,
+    authors: () => authors,
+  },
+
+  Book: {
+    author: (book) => {
+      return authors.find(({ id }) => id === book.authorId);
+    },
+  },
+
+  Author: {
+    books: (author) => {
+      return books.filter((book) => book.authorId === author.id);
+    },
   },
 };
 
